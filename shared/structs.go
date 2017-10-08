596699b37e7025c12606d007efdfcdf32c46239d
Michael Herold <michael.j.herold@gmail.com>
2017-10-08 12:45:06 -0500

    Extract file scanning into a reusable form

--- shared/structs.go
+++ shared/structs.go
@@ -1,5 +1,10 @@
 package shared
 
+import (
+	"archive/zip"
+	"os"
+)
+
 type File struct {
 	Path  string `json:"path"`
 	Error error  `json:"error,omitempty"`
@@ -14,6 +19,43 @@ type Scan struct {
 
 func NewScan(plugin, version string) *Scan {
 	return &Scan{plugin, version, []File{}}
+}
+
+func NewScanFromFile(plugin, version string, file *os.File) (*Scan, error) {
+	scan := NewScan(plugin, version)
+
+	stat, err := file.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	r, err := zip.NewReader(file, stat.Size())
+	if err != nil {
+		return nil, err
+	}
+
+	for _, f := range r.File {
+		if f.Name[len(f.Name)-1] == '/' && f.UncompressedSize64 == 0 {
+			continue
+		}
+
+		r, err := f.Open()
+		if err != nil {
+			scan.AddErrored(f.Name, err)
+			continue
+		}
+
+		hash, err := GetHash(r)
+		if err != nil {
+			scan.AddErrored(f.Name, err)
+			continue
+		}
+		r.Close()
+
+		scan.AddHashed(f.Name, hash)
+	}
+
+	return scan, nil
 }
 
 func (s *Scan) AddHashed(path string, hash uint32) {
